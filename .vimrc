ab41cc61d9a46412c9d47d971502247fa8a02a0e
akshay <akshay@void.localdomain>
2021-01-22 10:30:22 -0500

    First commit

---
+++ .vimrc
@@ -0,0 +1,18 @@
+execute pathogen#infect()
+
+syntax on
+
+set laststatus=2
+set nocompatible
+set nowrap
+set encoding=utf8
+set number
+set ruler
+set smarttab
+set expandtab
+set autoindent
+set mouse=a
+set tabstop=4
+set cursorline
+
+hi CursorLine term=bold cterm=bold ctermbg=black  guibg=Grey40
---
+++ suckless/dmenu-5.0/config.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#abb2bf", "#1d2026" },
+	[SchemeSel] = { "#1d2026", "#2f90d6" },
+	[SchemeSelHighlight] = { "#ffffff", "#2f90d6" },
+	[SchemeNormHighlight] = { "#be5046", "#1d2026" },
+	[SchemeOut] = { "#abb2bf", "#1d2026" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
